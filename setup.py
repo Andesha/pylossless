0ffdc24fd641ed861896a33306fac7f7101803d1
Tyler Collins <tk11br@sharcnet.ca>
2024-07-09 15:43:03 -0400

    first refactor of requirements

--- setup.py
+++ setup.py
@@ -13,18 +13,6 @@ from setuptools import setup, find_packages
 with Path("requirements.txt").open() as f:
     requirements = f.read().splitlines()
 
-extras = {
-    "dash": "requirements_qc.txt",
-    "test": "requirements_testing.txt",
-    "doc": "./docs/requirements_doc.txt",
-}
-
-extras_require = {}
-for extra, req_file in extras.items():
-    with Path(req_file).open() as file:
-        requirements_extra = file.read().splitlines()
-    extras_require[extra] = requirements_extra
-
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
@@ -40,7 +28,6 @@ setup(
     url="https://github.com/lina-usc/pylossless",
     packages=find_packages(),
     install_requires=requirements,
-    extras_require=extras_require,
     include_package_data=True,
     entry_points={"console_scripts": qc_entry_point},
 )
